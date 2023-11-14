7756b33bee66b916bdcc65f3dde6bb8832b2739d
Luke Kratz <lukeakratz@gmail.com>
2023-11-14 21:29:35 +0200

    Included a Time class to separate the main class from the Time class. The details of the time formatting is handled by the Time class.

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,12 @@ public class Main {
         // A program that converts the number of seconds in a day into a time, and can convert
         // the time into the number of seconds. The problem involves converting the
         // time given in String format into separate strings of hours, minutes and times.
-        String time;
-        int seconds;
-        int minutes;
-        int hours;
-        int totalSeconds;
-
-        // Convert the time into seconds
-        Scanner scannerObject = new Scanner(System.in);
-        System.out.println("Enter the time (hh:mm:ss): ");
-        // Obtain the time from the user.
-        time = scannerObject.next();
-
-        // Getting the number of seconds
-        char tensSecond = time.charAt(6);
-        char unitSecond = time.charAt(7);
-        String secondString = Character.toString(tensSecond) + Character.toString(unitSecond);
-        seconds = Integer.parseInt(secondString);
-
-        // Getting the number of minutes
-        char tensMinute = time.charAt(3);
-        char unitMinute = time.charAt(4);
-        String minuteString = Character.toString(tensMinute) + Character.toString(unitMinute);
-        minutes = Integer.parseInt(minuteString);
-
-        // Getting the number of minutes
-        char tensHour = time.charAt(0);
-        char unitHour = time.charAt(1);
-        String hourString = Character.toString(tensHour) + Character.toString(unitHour);
-        hours = Integer.parseInt(hourString);
-
-        // Convert the time to the number of seconds
-        totalSeconds = seconds + minutes * 60 + hours * 3600;
-        System.out.println("The total number of seconds: " + totalSeconds);
+        Time time = new Time();
+        time.readInput();
+        System.out.println("The total number of seconds for " + time);
+        String seconds = time.getSeconds();
+        String minutes = time.getMinutes();
+        String hours = time.getHours();
+        time.writeNumberOfSeconds(seconds, minutes, hours);
     }
 }
---
+++ src/Time.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class Time {
+    private String seconds;
+    private String minutes;
+    private String hours;
+    private int totalSeconds;
+
+    public Time(){
+        seconds = "0";
+        minutes = "0";
+        hours = "0";
+    }
+
+    public Time(String hours){
+        setTime("0", "0", hours);
+    }
+
+    public Time(String seconds, String minutes, String hours){
+        setTime(seconds, minutes, hours);
+    }
+
+    public Time(Time someTime){
+        if (someTime == null){
+            System.out.println("Fatal Error");
+            System.exit(0);
+        }
+        seconds = someTime.seconds;
+        minutes = someTime.minutes;
+        hours = someTime.hours;
+    }
+
+    public void setTime(String seconds, String minutes, String hours){
+        if (timeOK(seconds, minutes, hours)){
+            this.seconds = seconds;
+            this.minutes = minutes;
+            this.hours = hours;
+        }
+        else{
+            System.out.println("Fatal Error.");
+            System.exit(0);
+        }
+    }
+
+    public void setTime(String hours){
+        setTime("0", "0", hours);
+    }
+
+    public void setSeconds(String seconds){
+        this.seconds = seconds;
+    }
+
+    public void setMinutes(String minutes){
+        this.minutes = minutes;
+    }
+
+    public void setHours(String hours){
+        this.hours = hours;
+    }
+
+    public void setTotalSeconds(int totalSeconds){
+        this.totalSeconds = totalSeconds;
+    }
+
+    public String getSeconds(){
+        return seconds;
+    }
+
+    public String getMinutes(){
+        return minutes;
+    }
+
+    public String getHours(){
+        return hours;
+    }
+
+    private boolean timeOK(String secondsString, String minutesString, String hoursString){
+        int secondsBool = Integer.parseInt(secondsString);
+        int minutesBool = Integer.parseInt(minutesString);
+        int hoursBool = Integer.parseInt(hoursString);
+
+        return (((secondsBool >= 0) && (secondsBool <= 59)) &&
+                ((minutesBool >= 0) && (minutesBool <= 59) &&
+                        ((hoursBool >= 0) && (hoursBool <= 23))));
+    }
+
+    public void readInput(){
+        Scanner keyboard = new Scanner(System.in);
+        boolean tryAgain = true;
+        while(tryAgain){
+            System.out.println("Enter the time.");
+
+            System.out.print("Seconds: ");
+            String seconds = keyboard.next();
+
+            System.out.print("Minutes: ");
+            String minutes = keyboard.next();
+
+            System.out.print("Hours: ");
+            String hours = keyboard.next();
+
+            if (timeOK(seconds, minutes, hours)){
+                setTime(seconds, minutes, hours);
+                tryAgain = false;
+            }
+            else
+                System.out.println("Illegal time format. Please try again.");
+        }
+
+    }
+
+    public void writeNumberOfSeconds(String secondsString, String minutesString, String hoursString){
+        int seconds = Integer.parseInt(secondsString);
+        int minutes = Integer.parseInt(minutesString);
+        int hours = Integer.parseInt(hoursString);
+        totalSeconds = seconds + minutes * 60 + hours * 3600;
+
+        System.out.println("Total Seconds: " + totalSeconds);
+    }
+
+    public String toString(){
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+}
